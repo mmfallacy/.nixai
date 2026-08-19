9a21b245d966ea3ac60d095acacb8b9f83289eaf
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2026-08-19 11:39:15 +0000

    nix: shell: install pi-unwrapped

--- nix/devShell.nix
+++ nix/devShell.nix
@@ -2,16 +2,25 @@
   perSystem =
     { pkgs, extras, ... }:
     {
-      devShells.default = pkgs.mkShell {
-        name = ".nixai development shell";
+      devShells.default =
+        let
+          pi-unwrapped = pkgs.runCommand "pi-unwrapped" { } ''
+            mkdir -p $out/bin
+            ln -s ${pkgs.lib.getExe pkgs.pi-coding-agent} $out/bin/pi-unwrapped
+          '';
 
-        nativeBuildInputs = [
-          extras.mypkgs.ordna
-          (extras.mypkgs.pi-caelion.override {
-            agentConfigRoot = "~/dev/.nixai/pi";
-          })
-        ];
+        in
+        pkgs.mkShell {
+          name = ".nixai development shell";
 
-      };
+          nativeBuildInputs = [
+            pi-unwrapped
+            extras.mypkgs.ordna
+            (extras.mypkgs.pi-caelion.override {
+              agentConfigRoot = "~/dev/.nixai/pi";
+            })
+          ];
+
+        };
     };
 }
