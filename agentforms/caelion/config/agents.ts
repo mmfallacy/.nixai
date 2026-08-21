0565c1f73cdb500e3470dd6499aecfaf4982ef1e
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2026-08-21 20:03:28 +0000

    caelion: add config/*.ts that will be merged

---
+++ agentforms/caelion/config/agents.ts
@@ -0,0 +1,5 @@
+import type { Config } from "@opencode-ai/sdk/v2"
+
+export const agents = {
+  agent: {},
+} satisfies Pick<Config, "agent">
---
+++ agentforms/caelion/config/mcp.ts
@@ -0,0 +1,5 @@
+import type { Config } from "@opencode-ai/sdk/v2";
+
+export const mcp = {
+  mcp: {},
+} satisfies Pick<Config, "mcp">;
